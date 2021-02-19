c3392d726f86edc77c5d8a62b4684e40577e212f
ACETodd <34354353+ACETodd@users.noreply.github.com>
2021-02-19 12:33:02 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,92 @@
+
+//search 
+
+var inputId = 'searchbox';
+var itemsData = 'filter-value';
+var displaySet = false;
+var displayArr = [];
+
+
+
+
+
+
+
+
+
+
+function getDisplayType(element) {
+	var elementStyle = element.currentStyle || window.getComputedStyle(element, "");
+	return elementStyle.display;
+}
+
+document.getElementById(inputId).onkeyup = function() {
+	var searchVal = this.value.toLowerCase();
+	var filterItems = document.querySelectorAll('[' + itemsData + ']');
+
+
+
+	for(var i = 0; i < filterItems.length; i++) {
+		if (!displaySet) {
+			displayArr.push(getDisplayType(filterItems[i]));
+		}
+		
+		filterItems[i].style.display = 'none';
+
+
+		if(filterItems[i].getAttribute('filter-value').indexOf(searchVal) >= 0) {
+			filterItems[i].style.display = displayArr[i];
+
+			//document.getElementsByClassName('results')[0].style.display = "block";
+
+		}
+
+		
+	}
+	
+	displaySet = true;
+}
+
+
+//scroll to top
+
+const btnScrollToTop = document.getElementById("btnScrollToTop");
+
+btnScrollToTop.addEventListener("click", function() {
+
+	console.log("up");
+	window.scrollTo({
+		top: 0,
+		left: 0,
+		behavior: "smooth"
+	});
+});
+
+
+
+
+
+//vendor button
+
+// const btnStore = document.getElementsByClassName("storeButton");
+
+// function toggle() {
+// 	var blur = document.getElementById('blur');
+// 	blur.classList.toggle('active')
+// 	var popup = document.getElementsByClassName('popup');
+// 	popup.classList.toggle('active')
+// }
+
+
+// for (var i = 0; i < btnStore.length; i++) {
+// 	btnStore[i].addEventListener("click", function() {
+// 		toggle();
+// 		console.log("View Vendors");
+// 	})
+// }
+
+
+
+
+
+
